d17effd489b39dc8fcea50a153a6ab2a9311ead7
Wiznidhi <wiznidhi@gmail.com>
2022-10-31 13:01:56 +0530

    remade pool

--- src/UserPool.js
+++ src/UserPool.js
@@ -1,8 +1,8 @@
 import { CognitoUserPool } from "amazon-cognito-identity-js";
 
  const poolData = {
-     UserPoolId: "us-east-2_P3ZGjrRBS",
-     ClientId: "7pd89t7rovknl4b8ou9oo58dpt"
+     UserPoolId: "us-east-1_AvMC801Qj",
+     ClientId: "5ba4f1jacg9jg7siddtlit5bbo"
  }
  
  export default new CognitoUserPool(poolData); 
